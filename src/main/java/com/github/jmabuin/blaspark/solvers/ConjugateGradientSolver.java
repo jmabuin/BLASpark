50efff6df57eb3dc23ff7a84dbcf539a5f6814a5
jmabuin <chemaster@gmail.com>
2017-07-03 12:20:38 +0200

    Change in solver according last commit

--- src/main/java/com/github/jmabuin/blaspark/solvers/ConjugateGradientSolver.java
+++ src/main/java/com/github/jmabuin/blaspark/solvers/ConjugateGradientSolver.java
@@ -82,7 +82,7 @@ public class ConjugateGradientSolver {
 
 			//Inicio -- Ap=A*p
 			//Ap = L2.DGEMV(1.0, matrix, p, 0.0, Ap, jsc);
-			L2.DGEMV(1.0, matrix, p, 0.0, Ap, jsc);
+			Ap = L2.DGEMV(1.0, matrix, p, 0.0, Ap, jsc);
 
 			//Fin -- Ap=A*p
 
